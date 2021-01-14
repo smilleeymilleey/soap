3399c4303685ca4658dd512972c0b219eeac5d7e
Caitlyn <caitlynmiley.cm@gmail.com>
2021-01-14 12:30:45 -0600

    added the skeleton for an image carousel

--- soap/src/Home.js
+++ soap/src/Home.js
@@ -3,11 +3,43 @@ import "./Home.css";
 
 function Home() {
     return (
-        <div className="home">
-            <img className="lip__image" src="https://cdn.pixabay.com/photo/2018/06/25/13/00/cream-3496778_1280.jpg"></img>
-            <h1>FACE</h1>
-        </div>
-    )
-}
+        
+        <div className="slideshow-container">
+        
+          
+          <div className="mySlides fade">
+            <div className="numbertext">1 / 3</div>
+            <img src="img1.jpg" ></img>
+            <div className="text">Caption Text</div>
+          </div>
+        
+          <div className="mySlides fade">
+            <div className="numbertext">2 / 3</div>
+            <img src="img2.jpg"></img>
+            <div className="text">Caption Two</div>
+          </div>
+        
+          <div className="mySlides fade">
+            <div className="numbertext">3 / 3</div>
+            <img src="img3.jpg"></img>
+            <div className="text">Caption Three</div>
+          </div>
+        
+       
+          <a className="prev" onclick="plusSlides(-1)">&#10094;</a>
+          <a className="next" onclick="plusSlides(1)">&#10095;</a>
 
-export default Home
+        
+        
+        
+        <div>
+          <span className="dot" onclick="currentSlide(1)"></span>
+          <span className="dot" onclick="currentSlide(2)"></span>
+          <span className="dot" onclick="currentSlide(3)"></span>
+        </div>
+        
+       </div>
+    )
+};
+
+export default Home;
