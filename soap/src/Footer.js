e6f959588da794dea8e068eb3fa33593486d6212
Caitlyn <caitlynmiley.cm@gmail.com>
2021-02-11 23:21:14 -0600

    created a footer

--- soap/src/Footer.js
+++ soap/src/Footer.js
@@ -11,16 +11,24 @@ function Footer() {
             <div className="column">
                 <div className="Contact">
                     <h1>Contact</h1>
+                    <p>halogoddessforever@gmail.com</p>
+                    <p>(555)555-5555</p>
                 </div>
             </div>
           <div className="column">
             <div className="Contact">
                 <h1>About</h1>
+                <p>Charity</p>
+                <p>Blog</p>
+                <p>Our Story</p>
             </div>
         </div>
         <div className="column">
             <div className="Contact">
                 <h1> Social Media</h1>
+                <p>Instagram</p>
+                <p>Facebook</p>
+                <p>Twitter</p>
             </div>
         </div>
              
