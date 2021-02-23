21da835a3d29ad5f20bb59e31eb42b52dff1b42f
Caitlyn <caitlynmiley.cm@gmail.com>
2021-02-22 20:56:10 -0600

    react router bug fix

--- soap/src/About.js
+++ soap/src/About.js
@@ -8,7 +8,7 @@ function About() {
         <div>
           <h1 className="title">This is the About Page</h1>
             <h2> AbOUT US TEST</h2>
-            <Link to="\Users\Caitlyn Miley\soap\soap\soap\src\Home.js">Create Expense​</Link> 
+           
         </div>
     )
 }
